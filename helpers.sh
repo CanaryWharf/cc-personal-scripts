206304fb07c365f014293b7effb430a517a92b36
Raj Raj <raja.r184@gmail.com>
2020-12-25 19:26:21 +0000

    Add sync script to helpers

--- helpers.sh
+++ helpers.sh
@@ -1,10 +1,19 @@
 CC_ID_REGEX='^[0-9]+$'
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+function syncScripts() {
+    cp -r "$SCRIPTPATH/lib" "$1"
+}
+
 function syncToComputers() {
     for dir in $1/*/; do
-        BASE=$(basename $dir)
+        BASE=$(basename "$dir")
         if [[ $BASE =~ $CC_ID_REGEX ]]
         then
-            echo $dir
+            syncScripts "$dir"
         fi
     done
 }
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    syncToComputers "$@"
+fi
